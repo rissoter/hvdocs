23a64b5c1b10c5cbc8607c7a1c894d8341061f29
dul@FD-DEL-3080 <dul@du.aka>
2023-05-17 10:21:37 +0800

    update project setting

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'HVDocs'
+copyright = '2023, Rissoter'
+author = 'Rissoter'
 
 release = '0.1'
 version = '0.1.0'
